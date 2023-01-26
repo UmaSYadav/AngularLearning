570365cff7bf43f93131a61fe1af496cd65941e3
Uma Shankar Yadav <umashankar.yadav@avenues.info>
2023-01-26 19:55:09 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Home', component:HomeComponent},
+  {path:'About', component:AboutComponent},
+  {path:'Blogs', component:BlogsComponent},
+  {path:'Contact', component:ContactsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogsComponent } from './components/blogs/blogs.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { ProductComponent } from './components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogsComponent,
+    ContactsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule,
